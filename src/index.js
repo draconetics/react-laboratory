ca33ed51d83843f3bca738befcb8f84004606218
Mario Flores <dracon@hotmail.es>
2019-01-21 05:20:13 -0400

    setting to jsx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 const tasks = ['take out the trash','marco polo','another message'];
 
+//dont used
 const element = React.createElement('ol', null, 
     tasks.map((task, index)=> React.createElement('li',{key:index},task))
 );
 
-ReactDOM.render(element, document.getElementById('root'));
+class List extends Component{
+    render() {
+        return <ol>
+                {tasks.map((task,index) => <li key={index}>{task}</li>)}
+            </ol>
+    }
+}
+
+class Title extends Component {
+    render() {
+        return <h1> Task List</h1>;
+    }
+}
+
+class Main extends Component {
+    render() {
+        return <div> <Title/><List/> </div>;
+    }
+}
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
