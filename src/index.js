537b38a8717a197a277dab76eeec2ef769cf86d2
Mario Flores <dracon@hotmail.es>
2019-01-21 05:05:55 -0400

    adding key

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const tasks = ['take out the trash','marco polo','another message'];
 
 const element = React.createElement('ol', null, 
-    tasks.map((task)=> React.createElement('li',null,task))
+    tasks.map((task)=> React.createElement('li',{key:task},task))
 );
 
 ReactDOM.render(element, document.getElementById('root'));
