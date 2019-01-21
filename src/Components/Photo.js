26984c09d3fc65f6cc9e976567214b71564e5f49
Mario Flores <dracon@hotmail.es>
2019-01-21 09:48:31 -0400

    the idea is use only functions

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
 
 
+function Photo(props) {
+    const photo = props.post;
+        console.log(photo);
+        return  <figure className="figure">
+                    <img className="photo" src={photo.imageLink} alt={photo.description}/>
+                    <figcaption><p>{photo.description}</p></figcaption>
+                    <div className="button-container">
+                        <button className="remove-button">Remove</button>
+                    </div>
+                </figure>;
+}
 
+/*
 class Photo extends Component {
     render() {
         const photo = this.props.post;
@@ -15,5 +27,5 @@ class Photo extends Component {
                 </figure>;
     }
 }
-
+*/
 export default Photo;
