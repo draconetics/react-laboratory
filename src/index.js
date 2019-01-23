9dfcaf903b25a461d784db0c85071319d9715f39
Mario Flores <dracon@hotmail.es>
2019-01-23 05:33:01 -0400

    adding redux plugin

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './styles/stylesheet.css';
 import App from './Components/App';
 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>, document.getElementById('root'));
 
